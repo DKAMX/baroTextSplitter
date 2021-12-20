8de840b7779883e4d3f226e2d8d140366ad19260
Haokun <47777521+DKAMX@users.noreply.github.com>
2021-12-20 10:35:03 +0800

    add exception handling

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ A small program used for split text from Item element
 ## How to use 如何使用  
 1. Install JDK (version 11 or above, OpenJDK is also ok)  
    安装JDK（第11版本或者以上，OpenJDK也可以）  
-2. Run the `RUN_textsplit.bat` file  
+2. check that you have the `textsplit.java` file, this is the actual code that the program run  
+   Each time the program run, it will compile the source code again, to make sure it can run on this machine  
+   同时检查同目录下有`textsplit.java`和文件，这是程序实际运行的代码  
+   每次程序运行时，会重新编译一次源代码，确保程序能够在此电脑上正常运行  
+3. Run the `RUN_textsplit.bat` file  
    运行`RUN_textsplit.bat`文件  
-   Also check that you have the `textsplit.java` file, this is the actual code that the program run. Each time the `.bat` run, it will compile the sourcecode again, to make sure it can run on this machine  
-   同时检查同目录下有`textsplit.java`和文件，这是程序实际运行的代码，每次运行`.bat`批处理文件时，会重新编译一次源代码，确保程序能够在此电脑上正常运行  
-3. Temporaily not catch the Exception so when you type an incorrect filename, the program will close  
-   暂时没有进行异常处理，所以当你输入了一个错误的文件名，程序会自动结束
-## XML files that acceptable 可以接受的XML文件  
-Currently this program can only process files that relate to `Item` element  
-目前程序只能处理和`Item`有关的XML文件
+## Notice 注意事项  
+Currently this program can only process XML files that relate to `Item` element  
+Temporaily not catch the Exception so when you type an incorrect filename, the program will close  
+
+目前程序只能处理和`Item`有关的XML文件  
+暂时没有进行异常处理，所以当你输入了一个错误的文件名，程序会自动结束
--- textsplit.java
+++ textsplit.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,11 +15,11 @@ import org.w3c.dom.NodeList;
  * @version 0.1
  */
 public class textsplit {
-    static String welcome = "欢迎使用潜渊症文本分离器! Code by DKAMX\n可用指令：读取XML文件[file] 结束程序[end]";
-    static String askfile = "将文件和本程序放在同一层目录下，输入文件的名称（包括后缀名.xml）";
+    static String welcome = "欢迎使用潜渊症文本分离器! Code by DKAMX\n可用指令: 读取XML文件[file] 结束程序[end]";
+    static String askfile = "将文件和本程序放在同一层目录下，输入文件的名称(包括后缀名.xml)";
     static String donefile = "在同目录下输出了一个名为output的XML文件";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { // throws as methods demand
         String typing;
         BufferedReader echo = new BufferedReader(new InputStreamReader(System.in));
         do {
@@ -29,38 +30,44 @@ public class textsplit {
                 String filepath = echo.readLine();
                 filepath = filepath.replaceAll("\\\\", "/");
 
-                // setup a parser for XML
-                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                Document doc = db.parse(new File(filepath));
+                try {
+                    // setup a parser for XML
+                    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder db = dbf.newDocumentBuilder();
+                    Document doc = db.parse(new File(filepath));
 
-                // setup for writing file
-                File output = new File("output.xml");
-                FileOutputStream fOut = new FileOutputStream(output);
-                OutputStreamWriter writer = new OutputStreamWriter(fOut, "UTF-8");
+                    // setup for writing file
+                    File output = new File("output.xml");
+                    FileOutputStream fOut = new FileOutputStream(output);
+                    OutputStreamWriter writer = new OutputStreamWriter(fOut, "UTF-8");
 
-                // read from the NodeList
-                NodeList items = doc.getDocumentElement().getChildNodes();
-                // iterate NodeList and write values
-                writer.append("<infotexts>\n");
-                for (int i = 0; i < items.getLength(); i++) {
-                    // avoid #text node (line feed)
-                    if (!items.item(i).getNodeName().equals("#text")) {
-                        String name = items.item(i).getAttributes().getNamedItem("name").getNodeValue();
-                        String identifier = items.item(i).getAttributes().getNamedItem("identifier").getNodeValue();
-                        String description = items.item(i).getAttributes().getNamedItem("description").getNodeValue();
-                        System.out.println(
-                                "name: " + name + ", identifier:" + identifier + ", description:" + description);
-                        writer.append(
-                                "    <entityname." + identifier + ">" + name + "</entityname." + identifier + ">\n");
-                        writer.append("    <entitydescription." + identifier + ">" + name + "</entitydescription."
-                                + identifier + ">\n");
+                    // read from the NodeList
+                    NodeList items = doc.getDocumentElement().getChildNodes();
+                    // iterate NodeList and write values
+                    writer.append("<infotexts>\n");
+                    for (int i = 0; i < items.getLength(); i++) {
+                        // avoid #text node (line feed)
+                        if (!items.item(i).getNodeName().equals("#text")) {
+                            String name = items.item(i).getAttributes().getNamedItem("name").getNodeValue();
+                            String identifier = items.item(i).getAttributes().getNamedItem("identifier").getNodeValue();
+                            String description = items.item(i).getAttributes().getNamedItem("description").getNodeValue();
+                            System.out.println(
+                                    "name: " + name + ", identifier:" + identifier + ", description:" + description);
+                            writer.append(
+                                    "    <entityname." + identifier + ">" + name + "</entityname." + identifier
+                                            + ">\n");
+                            writer.append("    <entitydescription." + identifier + ">" + name + "</entitydescription."
+                                    + identifier + ">\n");
+                        }
                     }
+                    writer.append("</infotexts>");
+                    writer.close();
+                    fOut.close();
+                    System.out.println(donefile);
+                } catch (IOException except) { // handle for input wrong filename
+                    System.out.println(except.getLocalizedMessage());
+                    continue;
                 }
-                writer.append("</infotexts>");
-                writer.close();
-                fOut.close();
-                System.out.println(donefile);
             }
         } while (!typing.equalsIgnoreCase("end"));
     }
